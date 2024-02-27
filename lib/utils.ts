6a04b5e7bfaf9ad9ff58f3a3b252b9230be539c6
Donghyun Lee <leedonghyun022@gmail.com>
2024-02-27 13:11:56 +0900

    refactor: 성능 개선

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,13 +5,17 @@ export const initializeBrowserHistory = () => {
 };
 
 export const matchDynamicRoute = (path: string, pathname: string) => {
-  if (path.includes("/:")) {
-    const pathes = path.split("/");
-    const pathnames = pathname.split("/");
-    return (
-      pathes.length === pathnames.length &&
-      pathnames.every((v, i) => v === pathes[i] || pathes[i].startsWith(":"))
-    );
+  const isDynamicRoute = /\/:/g.test(path);
+  if (isDynamicRoute) {
+    const pathes = path.match(/[^/]+/g) ?? [];
+    const pathnames = pathname.match(/[^/]+/g) ?? [];
+    if (pathes.length !== pathnames.length) return false;
+    for (let i = 0; i < pathes.length; i++) {
+      if (pathes[i] !== pathnames[i] && !pathes[i].startsWith(":")) {
+        return false;
+      }
+    }
+    return true;
   }
   return path === pathname;
 };
