4ae02dede78bfd1a3d1f455b0f02f972b99e6082
Donghyun Lee <leedonghyun022@gmail.com>
2024-12-07 11:46:58 +0900

    feat: 타입 에러 개선

--- lib/types.ts
+++ lib/types.ts
@@ -1,11 +1,13 @@
+import { ReactNode } from "react";
+
 export interface History {
   pathname: string;
   query?: Record<string, string>;
 }
 
 export interface RouterProps {
-  routes: Record<string, () => JSX.Element> & Record<"/404", () => JSX.Element>;
-  children?: (Page: () => JSX.Element) => JSX.Element;
+  routes: Record<string, () => ReactNode> & Record<"/404", () => ReactNode>;
+  children?: (Page: () => ReactNode) => ReactNode;
 }
 
 export interface Router {
