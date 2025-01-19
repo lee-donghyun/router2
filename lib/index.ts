8dcc2018e42de1f301af7d0c7db30ff4c28cfee4
Donghyun Lee <leedonghyun022@gmail.com>
2025-01-19 17:50:46 +0900

    refactor: remove unused scroll restoration function from BrowserRouter

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,4 @@
 export { BrowserRouter } from "./components/BrowserRouter";
+export { disableBrowserScrollRestoration } from "./components/BrowserRouter.helper";
 export { Link } from "./components/Link";
 export { useRouter } from "./hooks/useRouter";
