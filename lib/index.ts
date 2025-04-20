b8c6667de6073c0d263c5346d2b1202b4c655fc0
Donghyun Lee <leedonghyun022@gmail.com>
2025-04-20 15:12:01 +0900

    feat: matchdnamicroue export 추가

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,5 @@
 export { BrowserRouter } from "./components/BrowserRouter";
 export { disableBrowserScrollRestoration } from "./components/BrowserRouter.helper";
 export { Link } from "./components/Link";
+export { matchDynamicRoute } from "./components/Router.helper";
 export { useRouter } from "./hooks/useRouter";
